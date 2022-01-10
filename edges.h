61e422f956b46467b902c4109fda4c6e8e2b2f57
naelaboraya <94143804+naelaboraya@users.noreply.github.com>
2022-01-10 23:48:32 +0200

    Update edges.h

--- edges.h
+++ edges.h
@@ -1,6 +1,6 @@
 #include "graph.h"
 
 void add_edge(pnode Src, pnode Dest, int Weight);
-void delete_edges(pnode head, pnode n);
-void delete_out_edges(pnode n);
-void printEdges(pnode node);
+void remove_edge(pnode head, pnode n);
+void remove_out(pnode n);
+void print_edges(pnode node);
