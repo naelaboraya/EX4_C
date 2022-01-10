67d89e46ab00ef4474b357058e5dcb1caf4de192
naelaboraya <94143804+naelaboraya@users.noreply.github.com>
2022-01-10 23:51:26 +0200

    Update nodes.h

--- nodes.h
+++ nodes.h
@@ -3,14 +3,14 @@
 
 
 
-pnode get_node(pnode NODE, int ID)
+pnode get_node(pnode NODE, int ID);
   
-void insert_node_cmd(pnode *head)
+void insert_node_cmd(pnode *head);
   
-void delete_node_cmd(pnode *head)
+void delete_node_cmd(pnode *head);
   
-void printGraph_cmd(pnode head)
+void printGraph_cmd(pnode head);
   
-void deleteGraph_cmd(pnode *head)
+void deleteGraph_cmd(pnode *head);
   
-char build_graph_cmd(pnode *head)
+char build_graph_cmd(pnode *head);
