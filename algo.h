938bfca659bd0b294017884090160a6051778620
naelaboraya <94143804+naelaboraya@users.noreply.github.com>
2022-01-10 23:25:15 +0200

    Update algo.h

--- algo.h
+++ algo.h
@@ -1,6 +1,11 @@
 #include "graph.h"
 
-
+int maximum_key(pnode NODE);
+bool is_empty(int is_v[], int length);
+int Minimum(int dist[], int is_v[], int length);
+int Dijkstra_Algorithm(pnode head, int src, int dest);
 void shortsPath_cmd(pnode head);
-
+int *delete_from_array(int arr[], int length, int k);
+void printArr(int arr[], int length);
+int helper(pnode head, int src, int arr[], int length);
 void TSP_cmd(pnode head);
